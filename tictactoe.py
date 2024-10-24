2123fff1c6869f419acdc3a64ca6cfa514d4951b
tqh23001 <truong.q.ho2006@gmail.com>
2024-10-24 14:48:24 -0400

    completed tic tac toe

--- tictactoe.py
+++ tictactoe.py
@@ -1,88 +1,96 @@
-import re
+grid = [['1' , '2', '3'], # The player grid
+        ['4', '5', '6'],
+        ['7', '8' , '9']]
 
-grid = [['1' , '2', '3' ],
-        ['4', '5', '6' ],
-    	['7', '8' , '9' ]]
+def reset_grid():
+    grid = [['1' , '2', '3'], # The player grid
+        ['4', '5', '6'],
+        ['7', '8' , '9']]
 
-def print_grid():
-    grid2 = ""
-    for i, row in enumerate(grid):
-        if i % 3 == 0 and i != 0:
-            grid2 += "\n "
-        grid2 += " ".join(map(str, row)) + '\n'
-    print(grid2)
-
-def edit_grid(player_choice):
-    for i, row in enumerate(grid):
-        for j, value in enumerate(row):
-            if value == player_choice:
-                grid[i][j] = 'x'
-                break
-
-def check_horiz():
-    count = 0
+def print_grid(): # Prints out the player grid
+    count = 0 # When to print new line
     for row in grid:
-        for value in row:
-            if value == 'x':
-                count += 1
-    if count == 3:
-       return 3
-    elif count == 0:
-        return 0
+        for element in row:
+            print(f'{element} ', end=' ')
+        print()
+
+def mod_grid(choice, turn): #Changes the grid based on player input
+    for i, row in enumerate(grid):
+        if choice in row:
+            y = row.index(choice) #Index of player choice inside the sublist where the player choice is found
+            if turn == 0:
+                grid[i][y] = 'X'
+            else:
+                grid[i][y] = 'O'
+
+def check_horiz(player): #Check horizontal player is the current player ('X' or 'Y')
+    for row in grid:
+        if row.count(player) == 3:
+            return True
+    return False
+
+def check_vert(player):
+    for col in range(3):
+        if grid[0][col] == grid[1][col] == grid[2][col]:
+            return True
+    return False
+
+def check_cross(player): #Checks across the board:
+    if grid[0][0] == grid[1][1] == grid [2][2]:
+        return True
+    elif grid[2][2] == grid[1][1] == grid [0][0]:
+        return True
     else:
-        return 1
-
-def chekc_vert():
-    count = 0
-    for i in len(grid):
-            if grid[i][0] == 'x':
-                count += 1
-    if count == 3:
-        return 3
-    elif count == 0:
-        return 0
-    else:
-        return 1
-
-def check_diag():
-    count = 0
-    if grid[0][0] == 'x':
-        count += 1
-        if grid[1][1] == 'x':
-            count += 1
-            if grid[2][2] == 'x':
-                count += 1
-    if count == 3:
-        return 3
-    elif count == 0:
-        return 0
-    else:
-        return 1
-    
-
-                
-
-def main():
-    done = False
-    turn = 0
-    count = 0
-    print('Welcome to Tic Tac Toe!')
-    while not done:
-         print(print_grid())
-         player = input("Player 1, please choose your grid(you are 'x'):")
-         if len(player) == 0 or player.isnumeric() == False:
-             print('Please enter something or a number!')
-             count
-         else:
-             edit_grid(player)
-             print(print_grid())
-             num = check_horiz()       
-             if num == 3:
-                 print('X wins!')      
-
-             
-    
+        return False
         
+def game():
+    end = False
+    players = {0: 'X', 1:'Y'} #Which player's turn, 0 for X and 1 for O
+    curr = 0 #Current player
+    last_play = 1 #Last player
+    play_count = 0
+
+    while not end:
+        choice = input(f'\nEnter a number on the grid to choose your turn! Player {players[curr]}\'s turn ')
+        while len(choice) != 1 or not choice.isdigit():
+            print('Please enter only one number')
+            choice = input()
+        if curr == 0:
+            mod_grid(choice, curr)
+            print_grid()
+            last_play = curr
+            curr = 1
+            play_count += 1
+        else:
+            mod_grid(choice, curr)
+            print_grid()
+            last_play = 1
+            curr = 0
+            play_count += 1
+        if play_count >= 3:
+            if check_vert(players[last_play]):
+                print(f'Player {players[last_play]} has won!')
+                end = True
+            elif check_horiz(players[last_play]):
+                print(f'Player {players[last_play]} has won!')
+                end = True
+            elif check_cross(players[last_play]):
+                print(f'Player {players[last_play]} has won!')
+                end = True
+        if play_count == 9:
+            print('Tie!')
+            continuee = input('Would you like to play again?').lower()
+            if continuee == 'n':
+                end = True
+            elif continuee == 'y':
+                reset_grid()        
+    
+def main():
+    print('Welcome to Tic Tac Toe!')
+    print_grid()
+    print()
+    game()
+
+   
 if __name__ == "__main__":
     main()
-    #print(re.sub(r']','\n', str(grid)).re.sub(r'[,','',str(grid)))
